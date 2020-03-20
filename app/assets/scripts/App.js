17ef48986bc6ca62f12514fdbbce618079093bb7
Rommel <rrosendo1991@gmail.com>
2020-03-20 15:07:18 -0700

    Completed our built process config.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -11,19 +11,19 @@ new MobileMenu();
 let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
-    el.addEventListener("click", e => {
-        e.preventDefault()
-        if (typeof modal == "undefined") {
-            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
-                modal = new x.default()
-                setTimeout(() => modal.openTheModal(), 20)
-            }).catch(() => console.log("There was a problem."))
-        } else {
-            modal.openTheModal()
-        }
-    })
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
 })
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
